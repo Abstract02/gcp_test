51d9356b59e14f682c2d62ae9fb0ab6dba59b563
Abstract02 <adarsh526222@gmail.com>
2024-03-30 04:52:44 +0530

    server commit

---
+++ api/index.js
@@ -0,0 +1,31 @@
+const cors = require("cors")
+const express = require("express")
+const App = express()
+
+
+App.use(express.json());
+App.use(cors())
+App.use(express.urlencoded({ extended: false }));
+
+
+// const mongoose = require("mongoose");
+
+// mongoose.connect("mongodb+srv://Abstract02:Bvdu4321@cluster0.hovn3.mongodb.net/?retryWrites=true&w=majority", {
+//     useNewUrlParser: true,
+// })
+// const connection = mongoose.connection;
+// connection.once('open', () => { console.log(`mongo db connected successfully`) })
+// const taskSchema = new mongoose.Schema({
+//     title: String,
+//     completed: Boolean
+// })
+
+// const Task = mongoose.model("Task", taskSchema);
+
+App.get("/todos", (req, res) => {
+   res.send("All is well");
+})
+
+
+const port = process.env.PORT || 5000
+App.listen(port,()=>{console.log("Server started")})
